2903fe0a50ddde7b46bca967eabb919923af285d
Kacper <kacper.krol@itsquad.pl>
2025-04-17 00:19:38 +0200

    Add project files.

---
+++ Tests/Tests/1768-MergeStringsAlternatelyTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using letcode_solutions.problems;
+using Xunit;
+
+
+namespace Tests.tests
+{
+    public class MergeAlternatelySolutionTests
+    {
+        [Theory]
+        [InlineData("abc", "pqr", "apbqcr")]
+        [InlineData("abc", "pqr", "apbqcr")]
+        [InlineData("abc", "pqr", "apbqcr")]
+        public void MergeAlternately_ReturnsCorrectResult(string a, string b, string expected)
+        {
+            var result = MergeAlternatelySolution.MergeAlternately(a, b);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ letcode-solutions/Problems/1768-MergeStringsAlternately.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letcode_solutions.problems
+{
+    public class MergeAlternatelySolution
+    {
+        public static string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder builder = new StringBuilder();
+            int length1 = word1.Length;
+            int length2 = word2.Length;
+            int maxLength = Math.Max(length1, length2);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < length1) builder.Append(word1[i]);
+                if (i < length2) builder.Append(word2[i]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
