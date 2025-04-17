0fa3c9cd2bb82b162c80cacc5345987d3e52fe8b
Kacper <kacper.krol@itsquad.pl>
2025-04-17 12:40:00 +0200

    feat: 345-ReverseVowels

---
+++ letcode-solutions/Problems/345-ReverseVowels.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letcode_solutions.Problems
+{
+    public class ReverseVowelsSolution
+    {
+        public static string ReverseVowels(string s)
+        {
+            char[] result = s.ToCharArray();
+            HashSet<char> chars = new HashSet<char>()
+            {
+            'a', 'e', 'i', 'o', 'u',
+            'A', 'E', 'I', 'O', 'U'
+            };
+
+            int i = 0;
+            int j = s.Length - 1;
+
+            while(i<j)
+            {
+                while(i<j && !chars.Contains(s[i]) )
+                {
+                    i++;
+                }
+
+                while (i < j && !chars.Contains(s[j]))
+                {
+                    j--;
+                }
+
+                char swap = s[i];
+                char swap2 = s[j];
+                result[i] = swap2;
+                result[j] = swap;
+
+                i++;
+                j--;
+
+            }
+
+            return new string(result);
+        }
+    }
+}
