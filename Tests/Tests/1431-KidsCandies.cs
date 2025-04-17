6c2da376435c5c12fd0709739cb323cc62d26a57
Kacper <kacper.krol@itsquad.pl>
2025-04-17 12:03:54 +0200

    feat: add kids candies [1431]

---
+++ Tests/Tests/1431-KidsCandies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using letcode_solutions.problems;
+using letcode_solutions.Problems;
+using Xunit;
+
+
+namespace Tests.tests
+{
+    public class KidsCandiesTests
+    {
+        [Theory]
+        [InlineData(new int[] { 12, 1, 12 }, 10, new bool[] {true, false, true })]
+        public void KidsCandies_ReturnsCorrectResult(int[] candies, int extracandies, bool[] expected)
+        {
+            var result = KidsWithCandiesSolution.KidsWithCandies(candies, extracandies);
+            Assert.Equal(expected, result.ToArray());
+        }
+    }
+}
---
+++ letcode-solutions/Problems/1431-KidsCandies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letcode_solutions.Problems
+{
+    public class KidsWithCandiesSolution
+    {
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            return candies.Select(x => x + extraCandies >= candies.Max()).ToList();
+        }
+    }
+}
